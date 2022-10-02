f19f6e916aa3bcdc87fd7de3bb011798907c087b
Steve Tu <stevetu11@yahoo.com>
2022-10-01 18:05:00 -0700

    Load in data from the JSON files

---
+++ android/app/src/main/java/com/steve1316/genshin_inventory_scanner_android/data/Data.kt
@@ -0,0 +1,11 @@
+package com.steve1316.genshin_inventory_scanner_android.data
+
+class Data {
+	companion object {
+		val artifactSets: ArrayList<String> = arrayListOf()
+		val characterDevelopmentItems: ArrayList<String> = arrayListOf()
+		val materials: ArrayList<String> = arrayListOf()
+		val weapons: ArrayList<String> = arrayListOf()
+		val characters: ArrayList<String> = arrayListOf()
+	}
+}
--- android/app/src/main/java/com/steve1316/genshin_inventory_scanner_android/utils/JSONParser.kt
+++ android/app/src/main/java/com/steve1316/genshin_inventory_scanner_android/utils/JSONParser.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
 import com.steve1316.genshin_inventory_scanner_android.MainActivity.loggerTag
+import com.steve1316.genshin_inventory_scanner_android.data.Data
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.File
@@ -36,6 +37,9 @@ class JSONParser {
 			}
 		} catch (e: Exception) {
 		}
+
+		// Now load in the data from all of the JSON files.
+		loadData(myContext)
 	}
 
 	/**
@@ -54,5 +58,68 @@ class JSONParser {
 		}
 
 		return newArrayList
+	}
+
+	/**
+	 * Load in the data from the JSON files in the /assets/data/ folder.
+	 *
+	 * @param myContext The application context.
+	 */
+	private fun loadData(myContext: Context) {
+		// Load data for artifact sets.
+		val artifactString = myContext.assets?.open("data/artifacts.json")?.bufferedReader().use { it?.readText() } ?: throw Exception("Could not load map data from the artifacts.json file.")
+		val artifactJSONArray = JSONArray(artifactString)
+		Data.artifactSets.clear()
+		for (i in 0 until artifactJSONArray.length()) {
+			val artifactSet = artifactJSONArray.getJSONObject(i)
+			Data.artifactSets.add(artifactSet["name"] as String)
+		}
+
+		Log.d(loggerTag, "Loaded in data for ${Data.artifactSets.size} artifact sets.")
+
+		// Load data for character development items.
+		val characterDevelopmentItemsString = myContext.assets?.open("data/characterdevelopmentitems.json")?.bufferedReader().use { it?.readText() }
+			?: throw Exception("Could not load map data from the characterdevelopmentitems.json file.")
+		val characterDevelopmentItemsJSONArray = JSONArray(characterDevelopmentItemsString)
+		Data.characterDevelopmentItems.clear()
+		for (i in 0 until characterDevelopmentItemsJSONArray.length()) {
+			val characterDevelopmentItem = characterDevelopmentItemsJSONArray.getJSONObject(i)
+			Data.characterDevelopmentItems.add(characterDevelopmentItem["name"] as String)
+		}
+
+		Log.d(loggerTag, "Loaded in data for ${Data.characterDevelopmentItems.size} Character Development Items.")
+
+		// Load data for materials.
+		val materialString = myContext.assets?.open("data/materials.json")?.bufferedReader().use { it?.readText() } ?: throw Exception("Could not load map data from the materials.json file.")
+		val materialJSONArray = JSONArray(materialString)
+		Data.materials.clear()
+		for (i in 0 until materialJSONArray.length()) {
+			val material = materialJSONArray.getJSONObject(i)
+			Data.materials.add(material["name"] as String)
+		}
+
+		Log.d(loggerTag, "Loaded in data for ${Data.materials.size} materials.")
+
+		// Load data for weapons.
+		val weaponString = myContext.assets?.open("data/weapons.json")?.bufferedReader().use { it?.readText() } ?: throw Exception("Could not load map data from the weapons.json file.")
+		val weaponJSONArray = JSONArray(weaponString)
+		Data.weapons.clear()
+		for (i in 0 until weaponJSONArray.length()) {
+			val weapon = weaponJSONArray.getJSONObject(i)
+			Data.weapons.add(weapon["name"] as String)
+		}
+
+		Log.d(loggerTag, "Loaded in data for ${Data.weapons.size} weapons.")
+
+		// Load data for characters.
+		val characterString = myContext.assets?.open("data/characters.json")?.bufferedReader().use { it?.readText() } ?: throw Exception("Could not load map data from the characters.json file.")
+		val characterJSONArray = JSONArray(characterString)
+		Data.characters.clear()
+		for (i in 0 until characterJSONArray.length()) {
+			val character = characterJSONArray.getJSONObject(i)
+			Data.characters.add(character["name"] as String)
+		}
+
+		Log.d(loggerTag, "Loaded in data for ${Data.characters.size} characters.")
 	}
 }
